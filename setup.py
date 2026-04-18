79ddd75ffb52e291d14be59e612c328e92ed4d24
Shubham Srivastava <23ssrivast@gmail.com>
2026-04-18 18:19:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='npest',
+    version='0.1.0',
+    py_modules=['npest'], # This tells pip to ONLY install the npest.py file
+    description='Non-parametric estimation tools',
+)
